b08b52c2465bf6248ad74718647dc7e0c2614e3d
ImaginaryDigital <jayteeid@gmail.com>
2015-12-01 19:12:01 -0500

    animation issue

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
 /*
  * ID Mock - r0.1.0
- * 2015-11-24 */
+ * 2015-11-28 */
 
  angular.module('app', [
-  'ngRoute'
+  'ngRoute',
+  'ngAnimate'
 ]);
 angular.module('app').config(function($interpolateProvider) {
   $interpolateProvider.startSymbol('{[{');
@@ -16,28 +17,61 @@ angular.module('app').config(function($interpolateProvider) {
 angular.module('app').config(['$routeProvider', function ($routeProvider){
   $routeProvider.when('/', {
     templateUrl: 'views/main.html',
-    controller: 'MainController'
+    controller: 'MainController',
+    animation: 'from-left'
   })
   .when('/photography', {
     templateUrl: 'views/photography.html',
-    controller: 'MainController'
+    controller: 'ViewController',
+    animation: 'from-right'
+
   })
   .when('/videos', {
     templateUrl: 'views/videos.html',
-    controller: 'MainController'
+    controller: 'ViewController',
+    animation: 'from-right'
   })
   .when('/design', {
     templateUrl: 'views/design.html',
-    controller: 'MainController'
+    controller: 'ViewController',
+    animation: 'from-right'
   })
   .otherwise({
     redirectTo: '/'
   });
 }]);
 
-function MainController ($scope){
+function MainController ($scope, $location){
 	$scope.hello = 'hello';
 	console.log($scope.hello);
 }
 
-angular.module('app').controller('MainController', ['$scope', MainController]);
+angular.module('app').controller('MainController', ['$scope', '$location', MainController]);
+
+
+function ViewController ($scope, $location) {
+	$scope.go = function() {
+	  $location.path('#/');
+	};
+}
+angular.module('app').controller('ViewController', ['$scope', '$location', ViewController]);
+
+
+
+// angular.module('app').controller('View2Controller', ['$scope', '$location',
+//   function MyCtrl($scope, $location) {
+//     $scope.go = function() {
+//       $location.path('/view1');
+//     };
+//   }
+// ]);
+
+angular.module('app').directive('viewAnimation', function ($route) {
+  return {
+    restrict: 'A',
+    link: function (scope, element) {
+      var animation = $route.current.animation;
+      if (animation) element.addClass(animation);
+    }
+  };
+});
